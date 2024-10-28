bc94476dc5a627eea056c0adf320c37fc421cbd3
Siya <tonjenisiya@gmail.com>
2024-10-28 16:06:59 +0200

    Adjusted Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-black text-white py-4">
       <div className="container mx-auto flex justify-between items-center font-epilogue">
-        <div className="text-lg font-bold">SIYA<span className='text-green-500'>.</span></div>
+        <div className="text-lg font-bold"><Link to='/'>SIYA<span className='text-green-500'>.</span></Link></div>
         <ul className="flex space-x-8">
           <li><Link to='/about' className="hover:text-gray-400">About</Link></li>
           <li><Link to='/projects' className="hover:text-gray-400 cursor-pointer">Work</Link></li>
