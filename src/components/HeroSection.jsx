98b5a8a310aa57c01eb5a66ee9d54be4ad44658b
Siya-Tonjeni <81377277+Siya-Tonjeni@users.noreply.github.com>
2024-10-31 13:47:14 +0200

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,6 @@ const HeroSection = () => {
     <section className="bg-white py-10 md:py-20">
       <div className="container mx-auto px-4 sm:px-6 flex flex-col md:flex-row items-center justify-between">
         
-        {/* Text Section */}
         <div className="w-full md:w-1/2 font-epilogue text-center md:text-left">
           <p className="mb-4 sm:mb-8 text-sm sm:text-base font-bold">
             Web Developer | UX/UI Designer 
@@ -20,7 +19,6 @@ const HeroSection = () => {
           </p>
         </div>
         
-        {/* Image Section */}
         <div className="w-full mt-8 md:mt-0 md:w-1/2 flex justify-center">
           <div className="relative w-3/4 sm:w-1/2 md:w-full">
             <img src={heroImage} alt="Hero Section" className="w-full h-auto" />
